db861594a92bdc46d85da3ce83a9b25a504aaf2a
shrishyla <43031870+shrishyla@users.noreply.github.com>
2018-09-20 16:17:33 +0530

    Add files via upload

---
+++ labs34.py
@@ -0,0 +1,13 @@
+marks=int(input("enter your marks"))
+if marks>=90:
+	print("your grade is A+")
+elif marks>=80:
+	print("your grade is A")
+elif marks>=70:
+	print("your grade is B+")
+elif marks>=60:
+	print("your grade is B")
+elif marks>=40:
+	print("your grade is C")
+else:
+	print("your grade is fail")
