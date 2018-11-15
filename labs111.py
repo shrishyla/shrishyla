ffa67650c3a264504cc0b25c69c3a833f765945a
shrishyla <43031870+shrishyla@users.noreply.github.com>
2018-11-15 16:16:26 +0530

    Add files via upload

--- labs111.py
+++ labs111.py
@@ -1,8 +1,8 @@
-import speach_recogonition as sr
-r = sr.Recogonizer()
-with sr.Microphone() as source
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
 	r.adjust_for_ambient_noise(source, duration=5)
 	print("say something")
 	while True:
 		audio=r.listen(source)
-		print("you said"+r.recogonise_google(audio))
+		print("you said"+r.recognize_google(audio))
--- labs1112.py
+++ labs1112.py
@@ -1,14 +1,13 @@
-import speach_recogonition as sr
+import speech_recognition as sr
 AUDIO_FILE=("example2.wav")
 #use the audio file as the audio source
-r = sr.Recogonizer()
-with sr.AudioFile(AUDIO_FILE) as source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
 #reads the audio file.here we use record intead of listen
-audio = r.record(source)
+	audio = r.record(source)
 try:
 	print("the audio file contains:"+r.recogonize_google(audio))
 except sr.UnknownValueError:
 	print("google speech recogonition could not understand audio")
 except sr.RequestError as e:
 	print("could not request results from google speech recogonition service:{0}".format(e))
-	
